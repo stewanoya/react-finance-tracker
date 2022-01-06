ea1c4abc9ffb0fe4d91dfc3a745da35a851ffa6f
Stew Anoya <stewartanoya@gmail.com>
2022-01-06 17:48:36 -0500

    refactor date to seperate component

---
+++ src/components/ExpenseDate.jsx
@@ -0,0 +1,17 @@
+const ExpenseDate = (props) => {
+  const { date } = props;
+
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <div>
+      <div>{day}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
+
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
 
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
