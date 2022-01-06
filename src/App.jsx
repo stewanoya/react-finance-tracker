0767531b1f1ab5a311ba40ae2b7bf8dc803b13f9
Stew Anoya <stewartanoya@gmail.com>
2022-01-06 17:48:42 -0500

    Add more test data

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ function App() {
       id: 1,
       title: "Car insurance",
       amount: 294.56,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 5, 11),
     },
     {
       id: 2,
       title: "New TV",
       amount: 123.24,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 7, 1),
     },
     {
       id: 3,
       title: "Rent",
       amount: 144364.06,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 1, 23),
     },
   ];
   return (
