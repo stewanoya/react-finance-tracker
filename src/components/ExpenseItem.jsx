505fe3bb12a9a94cdfa3f4fdc93c25c26ab5062a
Stew Anoya <stewartanoya@gmail.com>
2022-01-06 17:41:27 -0500

    use props from app

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,22 @@
+import "./ExpenseItem.css";
+
 const ExpenseItem = (props) => {
+  const { title, amount, date } = props;
+
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
-    <div>
-      <div>March 27, 2021</div>
+    <div className="expense-item">
       <div>
-        <h2>Car Payment</h2>
-        <div>$295.24</div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
