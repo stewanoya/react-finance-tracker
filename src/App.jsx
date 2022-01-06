30b95a0240439448ef3aea9f5dc50a0ded77e22d
Stew Anoya <stewartanoya@gmail.com>
2022-01-06 09:35:00 -0500

    Rename to jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <ExpenseItem />
+    </>
+  );
+}
+
+export default App;
