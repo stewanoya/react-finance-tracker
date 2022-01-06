0b7e4680ea079568a3ac5bd5cfc1c82b586e8667
Stew Anoya <stewartanoya@gmail.com>
2022-01-06 18:05:26 -0500

    change date orientation

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,3 +1,5 @@
+import "./ExpenseDate.css";
+
 const ExpenseDate = (props) => {
   const { date } = props;
 
@@ -6,10 +8,10 @@ const ExpenseDate = (props) => {
   const year = date.getFullYear();
 
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 };
