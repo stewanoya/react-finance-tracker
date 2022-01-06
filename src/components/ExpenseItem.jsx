98b615920a55be5aa7ee6381d61210bfe087236a
Stew Anoya <stewartanoya@gmail.com>
2022-01-06 09:36:02 -0500

    create components folder

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+const ExpenseItem = (props) => {
+  return (
+    <div>
+      <div>March 27, 2021</div>
+      <div>
+        <h2>Car Payment</h2>
+        <div>$295.24</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
